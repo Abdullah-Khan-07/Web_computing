c4cc0ffbce042cc99ff334ef81b259f48e8d67f0
Abdullah-Khan-07 <ka022824@gmail.com>
2025-10-24 21:09:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";  
+import BookStore from "./components/BookStore";
+
+function App() {
+  return (
+    <div>
+      <BookStore />
+    </div>
+  );
+}
+
+export default App;
